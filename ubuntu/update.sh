2229a4910cff6e98712c1f7fe0cef5f3085df10f
James Zimmerman II <james@comfreeze.net>
2017-01-02 17:54:57 -0800

    Integrating Ubuntu ISO's.

---
+++ ubuntu/update.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Script to refresh Ubuntu images
+# - This script downloads stable, beta and alpha channels of the CoreOS PXE iamges.
+# - REF URL: http://archive.ubuntu.com/ubuntu/dists/zesty/main/installer-amd64/current/images/netboot/
+#
+## Project
+TITLE="Ubuntu";
+SUBTITLE="ubuntu";
+## Mirror Definition
+ROOT_PATH="/ubuntu-iso";
+## Architecture targets
+AVAILABLE_ARCHES=(amd64);
+TARGET_ARCHES=(amd64);
+## Available Versions
+KNOWN_VERSIONS=(12.04.5 14.04.5 15.04 16.04.1 16.10);
+TARGET_VERSIONS=(12.04.5 14.04.5 15.04 16.04.1 16.10);
+## Extended Options
+EXTENDED_OPTIONS=(desktop server alternate)
+## Option Selection
+TARGET_OPTIONS=(desktop server alternate)
+## Directory Specifications
+CORE_DIR="${SUBTITLE}";
+## Save current directory
+START_DIR=$(pwd);
+## Output Generation
+OUTPUT_MENU_FILE="${START_DIR}/${CORE_DIR}/default.menu";
+## Generate Menu Header
+output_menu_header "${TITLE}" "${OUTPUT_MENU_FILE}";
+## Ensure base directory exists
+check_directory "${START_DIR}" "${CORE_DIR}";
+## Update selected versions
+for VERSION in ${TARGET_VERSIONS[@]}; do
+    MAJOR_VERSION=$(getrev "${VERSION}" . 1);
+#    box_line "Reading revision ${VERSION} - ${MAJOR_VERSION}"
+    ## Update each selected architecture
+    for ARCH in ${TARGET_ARCHES[@]}; do
+        # Create directory if necessary
+        check_directory "$(pwd)" "${VERSION}";
+#        ## Update each target option
+        for OPTION in ${TARGET_OPTIONS[@]}; do
+            URL="${PROTOCOL}://${BASE_HOST}${ROOT_PATH}/${VERSION}";
+            ## Compile the target filename
+            FILETARGET="ubuntu-${VERSION}-${OPTION}-${ARCH}.iso";
+            ## Download the target file
+            box_line "Fetching ${FILETARGET}"
+            getfile "${URL}/${FILETARGET}";
+            if [ -f "${FILETARGET}" ]; then
+                ## Generate menu entry
+                output_menu_entry_iso "${CORE_DIR}" "${VERSION}" "${ARCH}" "NA" "${FILETARGET}" "${TITLE}" "${OUTPUT_MENU_FILE}";
+                output_text_help "Boot ${TITLE} ${VERSION} ISO" "${OUTPUT_MENU_FILE}";
+            fi
+        done
+        cd ..
+    done
+done
+## Return to start directory
+cd ${START_DIR}
